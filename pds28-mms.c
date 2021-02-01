656cf1f8a028d4dfb0313b6013baa252e081a821
David Vukoje <davidv@thevtool.com>
2021-02-01 15:41:06 +0100

    Add poll functionality to driver

--- pds28-mms.c
+++ pds28-mms.c
@@ -10,6 +10,7 @@
 #include <linux/platform_device.h>
 #include <linux/of.h>
 #include <linux/string.h>
+#include <linux/poll.h>
 
 #define DEVICE_FILE_NAME	"pds28mms"
 #define DRIVER_NAME		"pds28mmsdrv"
@@ -42,10 +43,14 @@ struct pds28_mms {
 	struct device *parent;
 	struct cdev cdev;
 	dev_t devt;
+	int alarm;
 };
 
 /* global so it can be destroyed when module is removed */
 static struct class* pds28_mms_class;
+
+/* declare wait queue */
+static DECLARE_WAIT_QUEUE_HEAD(read_wq);
 
 /* convert 11-bit pds28_DEMO register value to 1/10th of a Celsius */
 static inline int pds28_mms_reg_to_mC(u16 val)
@@ -115,12 +120,30 @@ static ssize_t pds28_mmsdev_write(struct file *filp, const char __user *buf, siz
 	return -EINVAL;
 }
 
+
+static unsigned int pds28_mmsdev_poll(struct file *filp, poll_table *wait) 
+{
+	struct pds28_mms *mmsdev;
+	unsigned int retval_mask = 0;
+	mmsdev = filp->private_data;
+	poll_wait(filp, &read_wq, wait);
+	if (mmsdev->alarm) {
+		retval_mask = (POLLPRI | POLLRDNORM);
+		mmsdev->alarm = 0;
+	} else {
+		retval_mask = 0;
+	}
+	return retval_mask;
+}
+
+
 static struct file_operations pds28_mmsdev_fops = {
 	.owner = THIS_MODULE,
 	.open = pds28_mmsdev_open,
 	.release = pds28_mmsdev_release,
 	.read = pds28_mmsdev_read,
 	.write = pds28_mmsdev_write,
+	.poll = pds28_mmsdev_poll,
 };
 
 /*static ssize_t ien_show(struct device *child, struct device_attribute *attr, char *buf)
@@ -292,8 +315,9 @@ static irqreturn_t pds28_mms_isr(int irq, void *data)
 {
 	struct pds28_mms *mmsdev = data;
 
-	pr_info("ALARM received\n");
-
+	sysfs_notify(&mmsdev->dev->kobj, NULL, "data");
+	wake_up_interruptible(&read_wq);
+	mmsdev->alarm = 1;
 	iowrite32(0, mmsdev->base_addr + MMS_STATUS_OFFSET);
 
 	return IRQ_HANDLED;
